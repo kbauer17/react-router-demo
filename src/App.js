3a2ac3c1cd1e5a6e07f46c936539e65715925717
Kathy Bauer <kbauer17@my.wctc.edu>
2024-02-28 17:31:49 -0600

    [1.03] Create navigation using react-router-dom NavLink - Wrap entire App component in BrowserRouter component (index.js)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink } from 'react-router-dom';
 
 function App() {
+  const style = { paddingLeft: '.5rem' };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Router</h1>
+      <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem', }}>
+        <NavLink to="/home" style={style}>Home</NavLink>
+        <NavLink to="/products" style={style}>Products</NavLink>
+        <NavLink to="/about" style={style}>About</NavLink>
+      </nav>
     </div>
   );
 }

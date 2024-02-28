bc6468a04a054f8af5360ba27fd36f77fb64f0e0
Kathy Bauer <kbauer17@my.wctc.edu>
2024-02-28 17:37:31 -0600

    [1.07] Utilize NavLink's isActive property to conditionally render bo… …ld font on the active link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import About from './components/About';
 import Products from './components/Products';
 
 function App() {
-  const style = { paddingLeft: '.5rem' };
+  const style = ({ isActive }) => ({ 
+    paddingLeft: '.5rem',
+    fontWeight: isActive ? 'bold' : 'normal',
+  });
   return (
     <div>
       <h1>React Router</h1>

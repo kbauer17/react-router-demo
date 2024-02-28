9a62f81811483ecbe638d349ca28148ce33dc67c
Kathy Bauer <kbauer17@my.wctc.edu>
2024-02-28 17:34:02 -0600

    [1.04] create Home component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { NavLink } from 'react-router-dom';
+import Home from "./components/Home";
 
 function App() {
   const style = { paddingLeft: '.5rem' };
   return (
     <div>
       <h1>React Router</h1>
-      <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem', }}>
+      <nav style={{ 
+        borderBottom: 'solid 1px', 
+        paddingBottom: '1rem',
+        marginBottom: '1rem', 
+      }}>
         <NavLink to="/home" style={style}>Home</NavLink>
         <NavLink to="/products" style={style}>Products</NavLink>
         <NavLink to="/about" style={style}>About</NavLink>
       </nav>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <div>
+        This is the <strong>HOME</strong> page
+      </div>
+    );
+  }
+  
+  export default Home;
